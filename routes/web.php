5d24b0ffbf1e344916e099217e7960f0ab4a984a
dineshuprety <dineshuprety500@gmail.com>
2022-12-12 22:36:34 +0545

    tested user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('index',[
+        'user' => User::first()
+    ]);
 });
 
 Route::get('/about', function () {

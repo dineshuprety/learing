9ec19d472a9023be3e27c53528278e89b8dea3cb
dineshuprety <dineshuprety500@gmail.com>
2022-12-22 22:29:29 +0545

    worked on auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\StudentController;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +18,7 @@ use App\Http\Controllers\StudentController;
 |
 */
 // route::get('/',StudentController::class);
-Route::prefix('student')->group(function () {
+Route::prefix('student')->middleware('auth')->group(function () {
     Route::get('/', [StudentController::class, 'index'])->name('index');
     Route::get('/create', [StudentController::class, 'create'])->name('create');
     Route::post('/store', [StudentController::class, 'store'])->name('store');
@@ -23,3 +26,40 @@ Route::prefix('student')->group(function () {
     Route::post('/update/{student}', [StudentController::class, 'update'])->name('update');
     Route::get('/delete/{student}', [StudentController::class, 'delete'])->name('delete');
 });
+
+//auth route
+Route::get('/login', function(){
+    return view('login');
+})->name('login');
+
+// validate the user
+Route::post('/loging', function(Request $request){
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('index'));
+    }
+
+    return back()->withErrors([
+        'email' => 'The provided credentials do not match our records.',
+    ])->onlyInput('email');
+
+})->name('loging');
+
+
+//logout
+Route::get('/logout',function(Request $request){
+    Auth::logout();
+ 
+    $request->session()->invalidate();
+ 
+    $request->session()->regenerateToken();
+ 
+    return redirect(route('login'));
+})->name('logout');
+
